e3a545f2459bd7f317d7932629fb81db739b87bf
gkalstn000 <gkalstn000@gmail.com>
2022-05-18 12:40:45 +0900

    encoder 수정완료 input : (L, N, H_in) output : (L, N, H_out)

--- models/encoder.py
+++ models/encoder.py
@@ -4,14 +4,18 @@ import torch.nn.functional as F
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size, device):
+        # input_size : 2
+        # hidden_size : 10
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.gru = nn.GRU(input_size, hidden_size)
         self.device = device
 
     def forward(self, input, hidden):
-        output = input # (B, N, 2)
-        output, hidden = self.gru(output, hidden)
+        # input size : (L, N, H_in)
+        if len(input.size()) == 2 :
+            input = input.unsqueeze(1) # (L, N, H_in)
+        output, hidden = self.gru(input, hidden)
         return output, hidden
 
     def initHidden(self):
@@ -19,18 +23,17 @@ class EncoderRNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, device):
+        # hidden_size : 10
+        # output_size : 2
         super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
         self.device = device
 
     def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
+        output = F.relu(input)
         output, hidden = self.gru(output, hidden)
         output = self.softmax(self.out(output[0]))
         return output, hidden
@@ -46,7 +49,7 @@ if __name__ == '__main__' :
 
     key_points = data['fasionWOMENDressesid0000041606_7additional']
 
-    input_ = torch.Tensor(key_points).unsqueeze(0)
+    input_ = torch.Tensor(key_points)
 
     encoder = EncoderRNN(2, 10, device)
     encoder_hidden = encoder.initHidden()
