a5c4e0ff161702bd41255ec56c98f239b37e7c1b
gkalstn000 <gkalstn000@gmail.com>
2022-10-10 18:27:33 +0900

    s

--- train_script.sh
+++ train_script.sh
@@ -6,4 +6,8 @@ python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_10_nheads_10_lambdao_1 
 python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_10_nheads_10_lambdao_10 --embedding_dim 10 --n_layers 6 --d_k 10 --d_v 10 --n_heads 10 --lambda_bce 10 --gpu_ids 1
 python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_10_nheads_10_lambdao_50 --embedding_dim 10 --n_layers 6 --d_k 10 --d_v 10 --n_heads 10 --lambda_bce 50 --gpu_ids 1
 python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_10_nheads_10_lambdao_100 --embedding_dim 10 --n_layers 6 --d_k 10 --d_v 10 --n_heads 10 --lambda_bce 1=100 --gpu_ids 1
-python train.py --id 1010_embedding_dim_20_nlayers_20_dkv_10_nheads_20_lambdao_1 --embedding_dim 10 --n_layers 6 --d_k 10 --d_v 10 --n_heads 10 --lambda_bce 1 --gpu_ids 1
+python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_20_nheads_20_lambdao_1 --embedding_dim 10 --n_layers 6 --d_k 20 --d_v 20 --n_heads 20 --lambda_bce 1 --gpu_ids 1
+python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_20_nheads_20_lambdao_10 --embedding_dim 10 --n_layers 6 --d_k 20 --d_v 20 --n_heads 20 --lambda_bce 1 --gpu_ids 10
+python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_20_nheads_20_lambdao_50 --embedding_dim 10 --n_layers 6 --d_k 20 --d_v 20 --n_heads 20 --lambda_bce 1 --gpu_ids 50
+python train.py --id 1010_embedding_dim_10_nlayers_6_dkv_20_nheads_20_lambdao_100 --embedding_dim 10 --n_layers 6 --d_k 20 --d_v 20 --n_heads 20 --lambda_bce 1 --gpu_ids 100
+
